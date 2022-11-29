728b3ab350a4fe39253ef6e8ba3d7183ab05c546
Fredrick Njoki <njokifredrick1@gmail.com>
2022-11-29 23:12:04 +0000

    ICP

---
+++ RouteNode.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include<unordered_map>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+/*
+*
+*/
+class RouteNode{
+public:
+    string airportCode;
+    RouteNode *parent;
+    string airlineCode
+    int stops;
+};
