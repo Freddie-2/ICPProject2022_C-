3f0fd36eb1d30a633b3127250f61ffdd403ed9fd
Fredrick Njoki <njokifredrick1@gmail.com>
2022-11-30 01:12:41 +0000

    ICP

---
+++ search.cpp
@@ -0,0 +1,11 @@
+#include "Route.h"
+#include "RouteNode.h"
+
+using namespace std;
+
+vector<Routen>getRoutes(string airportCode, unordered_map<string, vector<Route>>& routes) {
+    vector<Route> destAirports;
+    for (auto Route : routes){
+        if (Route.)
+    }
+}
